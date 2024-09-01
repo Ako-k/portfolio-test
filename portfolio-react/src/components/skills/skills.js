5f5d3decfd58ee220a6d3cb65477bb1e33d6b556
Ako-k <fantasydia_ry@yahoo.co.jp>
2024-09-01 08:14:08 +0000

    add skills page

--- portfolio-react/src/components/skills/skills.js
+++ portfolio-react/src/components/skills/skills.js
@@ -4,6 +4,17 @@ function Skills() {
   return (
     <div>
       <p>Skills page</p>
+
+      <div>
+        <h3>【所持資格】</h3>
+        <ul>
+          <li>ITパスポート試験</li>
+          <li>基本情報技術者試験</li>
+          <li>応用情報技術者試験</li>
+          <li>Ruby Association Certified Ruby Programmer Silver version 3</li>
+          <li>Ruby Association Certified Ruby Programmer Gold version 3</li>
+        </ul>
+      </div>
     </div>
   );
 }
