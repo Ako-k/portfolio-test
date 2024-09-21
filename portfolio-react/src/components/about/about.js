ba5043abe524ff98f4e88cd84f55a6c73b387690
Ako-k <fantasydia_ry@yahoo.co.jp>
2024-09-21 08:09:01 +0000

    update about page

--- portfolio-react/src/components/about/about.js
+++ portfolio-react/src/components/about/about.js
@@ -1,30 +1,41 @@
 import React from 'react';
+import './about.css';
 
 function About() {
   return (
     <div>
       <p>About page</p>
 
-      <table>
-        <tr>
-          <th>名前</th>
-          <td>Ako</td>
-        </tr>
-        <tr>
-          <th>年齢</th>
-          <td>30</td>
-        </tr>
-        <tr>
-          <th>性別</th>
-          <td>男</td>
-        </tr>
-        <tr>
-          <th>職業</th>
-          <td>システムエンジニア</td>
-        </tr>
-
-
-      </table>
+      <div class='nes-table-responsive'>
+        <table class='nes-table is-bordered is-dark th-center-table'>
+          <tr>
+            <th>おなまえ</th>
+            <td>Ako</td>
+          </tr>
+          <tr>
+            <th>ねんれい</th>
+            <td>30</td>
+          </tr>
+          <tr>
+            <th>せいべつ</th>
+            <td>男</td>
+          </tr>
+          <tr>
+            <th>じょぶ</th>
+            <td>システムエンジニア</td>
+          </tr>
+          <tr>
+            <th>しょぞく</th>
+            <td>SJC</td>
+          </tr>
+          <tr>
+            <th>
+              <i class='nes-icon gmail'></i>
+            </th>
+            <td>aaa@sjc-inc.co.jp</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
